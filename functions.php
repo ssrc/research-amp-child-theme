219073787be3783288827cffa895739a80f05ef1
Boone B Gorges <boonebgorges@gmail.com>
2023-10-04 13:33:25 -0500

    Use a named function to enqueue stylesheets.

--- functions.php
+++ functions.php
@@ -9,13 +9,11 @@
 /**
  * Enqueues stylesheets.
  */
-add_action(
-	'wp_enqueue_scripts',
-	function() {
-		// Enqueue stylesheet from parent theme research-amp-theme.
-		wp_enqueue_style( 'research-amp-theme', get_template_directory_uri() . '/style.css' );
+function research_amp_child_theme_enqueue_styles() {
+	// Enqueue stylesheet from parent theme research-amp-theme.
+	wp_enqueue_style( 'research-amp-theme', get_template_directory_uri() . '/style.css' );
 
-		// Enqueue the stylesheet from the current theme.
-		wp_enqueue_style( 'research-amp-child-theme', get_stylesheet_uri(), array( 'research-amp-theme' ) );
-	}
-);
+	// Enqueue the stylesheet from the current theme.
+	wp_enqueue_style( 'research-amp-child-theme', get_stylesheet_uri(), array( 'research-amp-theme' ) );
+}
+add_action( 'wp_enqueue_scripts', 'research_amp_child_theme_enqueue_styles' );
